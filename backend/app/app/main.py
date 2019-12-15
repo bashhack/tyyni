e69a242780e2ecfaa9dbfabc676e85b5d4454189
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-15 15:52:52 -0500

    Add basic message on FastAPI init

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,3 +1,12 @@
 import sys
 
+from fastapi import FastAPI
+
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    message = f"Hello world! From FastAPI running on Uvicorn with Gunicorn in tyyni. Using Python {version}"
+    return {"message": message}

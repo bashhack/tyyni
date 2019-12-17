dc0ef4459c97f2ae4032488c92c10c26a746e048
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-16 21:15:30 -0500

    Add retry logic on DB session init

--- backend/app/app/backend_pre_start.py
+++ backend/app/app/backend_pre_start.py
@@ -1,16 +1,27 @@
 import logging
 
+from tenacity import retry, stop_after_attempt, wait_fixed, before_log, after_log
+
 from app.db.session import db_session
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+max_attempt_number = 60 * 5  # Five (5) minutes
+wait_time = 1  # One (1) second
 
+
+@retry(
+    stop=stop_after_attempt(max_attempt_number=max_attempt_number),
+    wait=wait_fixed(wait=wait_time),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
 def init():
     try:
         # Attempt to create a DB session,
         # this will serve to check if DB is awake
-        db_session.execute('SELECT 1')
+        db_session.execute("SELECT 1")
     except Exception as e:
         logger.error(e)
         raise e

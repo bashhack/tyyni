113965a45132540c366cfd907c5f5864852ed9b3
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-17 22:48:46 -0500

    Update decouple config for PostgreSQL URI

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -6,8 +6,8 @@ PROJECT_NAME = config("PROJECT_NAME", default=None)
 API_V1_STR = config("API_V1_STR", default=API_V1)
 OPENAPI_URL = config("OPENAPI_URL", default=f"{API_V1}/openapi.json")
 
-POSTGRES_USER = config("POSTGRES_USER", default=None)
-POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", default=None)
-POSTGRES_SERVER = config("POSTGRES_SERVER", default=None)
-POSTGRES_DB = config("POSTGRES_DB", default=None)
+POSTGRES_USER = config("POSTGRES_USER", default='postgres')
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", default='')
+POSTGRES_SERVER = config("POSTGRES_SERVER", default='db')
+POSTGRES_DB = config("POSTGRES_DB", default='app')
 SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"

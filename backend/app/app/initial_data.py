01598c0a8f5276755a451a3fd5b7b9fb12b1a7fa
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-18 23:31:11 -0500

    Update backend env example with SUPERUSER env vars

---
+++ backend/app/app/initial_data.py
@@ -0,0 +1,21 @@
+import logging
+
+from app.db.init_db import init_db
+from app.db.session import db_session
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init():
+    init_db(db_session)
+
+
+def main():
+    logger.info('Seeding initial data...')
+    init()
+    logger.info('...seed data created') 
+    
+
+if __name__ == "__main__":
+    main()

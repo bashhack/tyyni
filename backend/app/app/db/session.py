9ea7c6823c90f6487a6b9a19d53231a522118169
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-19 00:09:03 -0500

    Create database connection + session factory + session registry + documentation from SQLAlchemy

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -3,5 +3,22 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from app.core.config import SQLALCHEMY_DATABASE_URI
 
+# Create a database connection
 engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+# Create a registry of Session objects using a session factory instance via sessionmaker
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
+# Create a session factory instance (for use in executing model queries, etc.)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# NOTE: 
+#       `This pattern allows disparate sections of the application to call 
+#       upon a global scoped_session, so that all those areas may share the 
+#       same session without the need to pass it explicitly.`
+# 
+# For more on this pattern and the usages here, see:
+# https://docs.sqlalchemy.org/en/13/orm/contextual.html

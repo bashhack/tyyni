a5c39003b7fe17448c94c373b5f1ff39c5bd8271
bashhack (Marc Laughton) <info@marclaughton.com>
2019-12-18 23:43:03 -0500

    Add noqa to imports in db/base.py, so they are not removed

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -1,5 +1,5 @@
 # Imports here will be seen and imported by Alembic
 
-from app.db.base_class import Base
-from app.db_models.user import User
+from app.db.base_class import Base  # noqa
+from app.db_models.user import User  # noqa
 

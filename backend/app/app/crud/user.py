afdb68455328314136780538fe9553f076417482
Marc Laughton <info@marclaughton.com>
2019-12-21 23:26:12 -0500

    Adds `users` endpoint (#33)
    * Update env var for first superuser => first superuser email
    
    * Add DB session middleware
    
    * Add get_db handler
    
    * Add create_user, get_users, and get_user_by_email methods
    
    * Update comment formatting in db/session.py
    
    * Update init_db to use create_user and get_user_by_email
    
    * Needed to add orm_mode for SQLAlchemy
    
    * Formatting changes in db/base.py
    
    * Add basic get_users endpoint
    
    * Attach users mount to api

--- backend/app/app/crud/user.py
+++ backend/app/app/crud/user.py
@@ -1,10 +1,9 @@
-from app.db.session import Session
+from typing import List, Optional
+
+from sqlalchemy.orm import Session
+
 from app.db_models.user import User
 from app.models.user import UserCreate
-
-
-def get_user_by_email(db_session: Session, *, email: str):
-    return db_session.query(User).filter(User.email == email).first()
 
 
 def create_user(db_session: Session, *, user_in: UserCreate) -> User:
@@ -18,3 +17,11 @@ def create_user(db_session: Session, *, user_in: UserCreate) -> User:
     db_session.commit()
     db_session.refresh(user)
     return user
+
+
+def get_users(db_session: Session, *, skip=0, limit=100) -> List[Optional[User]]:
+    return db_session.query(User).offset(skip).limit(limit).all()
+
+
+def get_user_by_email(db_session: Session, *, user_email: str) -> Optional[User]:
+    return db_session.query(User).filter(User.email == user_email).first()

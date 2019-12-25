12210fa2c6478f3c89ed5b2c26d72ffafe0f10ac
Marc Laughton <info@marclaughton.com>
2019-12-24 22:51:05 -0500

    Use yield dependency pattern + add documentation around how the dependency works (#39)

--- backend/app/app/api/utils/db.py
+++ backend/app/app/api/utils/db.py
@@ -1,5 +1,24 @@
-from starlette.requests import Request
+from app.db.session import Session
 
 
-def get_db(request: Request):
-    return request.state.db
+def get_db():
+    """
+    
+    We need to have an independent database session/connection (Session) per request,
+    then use the same session throughout the request lifecycle and, finally,
+    close it after the request is finished.
+
+    Technical details:
+
+     - Only code prior to and including the yield statement is executed before sending a response.
+
+     - The yielded value is what is injected into path operations and other dependencies.
+    
+     - The code following the yield statement is executed after the response has been delivered.
+    """
+
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()

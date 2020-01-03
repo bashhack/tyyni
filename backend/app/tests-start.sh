40b83812f06b8df5188a6c106fcb76443e64b7f1
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-03 01:18:00 -0500

    Add Github links to pytest-watch bug that I'm experiencing

--- backend/app/tests-start.sh
+++ backend/app/tests-start.sh
@@ -6,6 +6,9 @@ export TESTING=True
 
 
 # NOTE: Using Internal Field Separator (IFS) expansion syntax...
-ptw --runner "pytest -s $* /app/app/tests"
+# ptw -w --runner "pytest -s $* /app/app/tests"
+# NOTE:
+#   - DB errors: https://github.com/joeyespo/pytest-watch/issues/23
+#   - Follow-up: https://github.com/joeyespo/pytest-watch/issues/36
 
-#pytest "$*" /app/app/tests
+pytest "$*" /app/app/tests
